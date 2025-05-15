4b466b24bf3e6e6b0adbe0fe2f0ca297c39d558d
bradallenfisher <bradallenfisher@gmail.com>
2025-05-14 22:32:06 -0400

    Update visactor.tsx

--- src/components/nav/side-nav/components/visactor.tsx
+++ src/components/nav/side-nav/components/visactor.tsx
@@ -1,6 +1,6 @@
 export default function VisActor() {
   return (
       <span className="text-xs text-muted-foreground">Powered by</span>
-        Brad Fucking Fisher
+        <div>Brad Fucking Fisher</div>
   );
 }
