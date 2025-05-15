e691186bcbb8416a4eaa4954dd3e6cd31e5088f1
bradallenfisher <bradallenfisher@gmail.com>
2025-05-14 22:30:46 -0400

    Update visactor.tsx

--- src/components/nav/side-nav/components/visactor.tsx
+++ src/components/nav/side-nav/components/visactor.tsx
@@ -1,18 +1,6 @@
-import Link from "next/link";
-
 export default function VisActor() {
   return (
-    <Link
-      href="https://visactor.io"
-      target="_blank"
-      className="relative my-2 flex flex-col items-center justify-center gap-y-2 px-4 py-4"
-    >
-      <div className="dot-matrix absolute left-0 top-0 -z-10 h-full w-full" />
       <span className="text-xs text-muted-foreground">Powered by</span>
-      <div className="flex items-center space-x-2">
         Brad Fucking Fisher
-        <span className="text-md text-accent-foreground">VisActor</span>
-      </div>
-    </Link>
   );
 }
